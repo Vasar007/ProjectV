55586b0af9e9cebe4b5273a4ffb496ec05faadb4
Данила <guseffdanila@ya.ru>
2019-01-26 13:03:24 +0300

    add reading from file

--- ThingAppraiser/Program.cs
+++ ThingAppraiser/Program.cs
@@ -9,8 +9,37 @@ namespace ThingAppraiser
 
         static void Main(string[] args)
         {
+            
             var crawler = new Crawlers.TMDBCrawler();
             var results = crawler.GetData(Movies);
+
+            var input = new input.Input();
+            string[] names = { };
+            try
+            {
+                names = input.ReadLines(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect file name");
+            }
+            while (names.Length == 0)
+            {
+                Console.WriteLine("input other file name");
+                try
+                {
+                    names = input.ReadLines(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect file name");
+                }
+                for(var i = 0; i < names.Length; ++i)
+                {
+                    Console.WriteLine(names[i]);
+                }
+            }
+
             foreach (var result in results)
             {
                 Console.WriteLine(JToken.FromObject(result));
---
+++ ThingAppraiser/input/Input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ThingAppraiser.input
+{
+    public class Input
+    {
+        public string[] ReadLines(string fileName)
+        {
+            List<string> res = new List<string>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Do stuff with your line here, it will be called for each 
+                    // line of text in your file.
+                    res.Add(line.Trim(new[] { '\r', '\n', ' ' }));
+                }
+            }
+            return res.ToArray();
+        }
+    }
+}
