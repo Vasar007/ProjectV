1d61500bc0dacb0fcc3cdcb2676945018986cc6a
Vasily Vasilyev <vasar007@yandex.ru>
2019-11-17 21:14:54 +0300

    Fix broken F# test

--- ThingAppraiser/Tests/ThingAppraiser.ContentDirectories.Tests/ContentFinderTests.fs
+++ ThingAppraiser/Tests/ThingAppraiser.ContentDirectories.Tests/ContentFinderTests.fs
@@ -60,7 +60,7 @@ let ``Directory sequence is null for "FindContentForDirWithAsync" call`` () =
     let seqGen = FileSeqGenerator.Sync(f)
 
     raises<ArgumentNullException>
-        <@ ContentFinder.FindContentForDirWithAsync null seqGen contentType @>
+        <@ ContentFinder.FindContentForDirWithAsync null seqGen contentType None @>
 
 [<Fact>]
 let ``Directory sequence is null for "FindContentAsync" call`` () =
