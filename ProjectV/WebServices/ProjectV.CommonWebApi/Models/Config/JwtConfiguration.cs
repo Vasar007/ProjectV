846f4dc6427c37175e68466997ff49d34b9f6cea
Vasily Vasilyev <vasar007@yandex.ru>
2022-05-03 17:39:19 +0200

    #90 move secret key from config to environment variable

--- ProjectV/WebServices/ProjectV.CommonWebApi/Models/Config/JwtConfiguration.cs
+++ ProjectV/WebServices/ProjectV.CommonWebApi/Models/Config/JwtConfiguration.cs
@@ -3,7 +3,11 @@
     // TODO: make this DTO immutable.
     public sealed class JwtConfiguration
     {
-        public string SecretKey { get; set; } = default!;
+        // Not use "string.Empty" to simplify insertion of hard-coded value.
+        private static readonly string _defaultSecretKey = "";
+
+        public string SecretKey { get; set; } =
+            EnvironmentVariablesParser.GetValueOrDefault("JwtSecretKey", _defaultSecretKey);
 
         public string Issuer { get; set; } = default!;
 
