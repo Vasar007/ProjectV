f7330bc7269fab841435185f5beb5748e72ad44a
Vasily Vasilyev <vasar007@yandex.ru>
2022-06-13 20:54:06 +0200

    Features: upgrade NuGet packages

--- ProjectV/Libraries/ProjectV.Core/DependencyInjection/ServiceCollectionExtensions.cs
+++ ProjectV/Libraries/ProjectV.Core/DependencyInjection/ServiceCollectionExtensions.cs
@@ -7,34 +7,30 @@ namespace ProjectV.Core.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddHttpClientWithOptions(
+        public static IHttpClientBuilder AddHttpClientWithOptions(
             this IServiceCollection services, HttpClientOptions options)
         {
             services.ThrowIfNull(nameof(services));
             options.ThrowIfNull(nameof(options));
 
-            services
+            return services
                 .AddHttpClient(options.HttpClientDefaultName)
                 .AddBuilderOptionsInternal(options);
-
-            return services;
         }
 
-        public static IServiceCollection AddHttpClientWithOptions<TClient>(
+        public static IHttpClientBuilder AddHttpClientWithOptions<TClient>(
            this IServiceCollection services, HttpClientOptions options)
            where TClient : class
         {
             services.ThrowIfNull(nameof(services));
             options.ThrowIfNull(nameof(options));
 
-            services
+            return services
                 .AddHttpClient<TClient>(options.HttpClientDefaultName)
                 .AddBuilderOptionsInternal(options);
-
-            return services;
         }
 
-        public static IServiceCollection AddHttpClientWithOptions<TClient, TImplementation>(
+        public static IHttpClientBuilder AddHttpClientWithOptions<TClient, TImplementation>(
             this IServiceCollection services, HttpClientOptions options)
             where TClient : class
             where TImplementation : class, TClient
@@ -42,20 +38,16 @@ namespace ProjectV.Core.DependencyInjection
             services.ThrowIfNull(nameof(services));
             options.ThrowIfNull(nameof(options));
 
-            services
+            return services
                 .AddHttpClient<TClient, TImplementation>(options.HttpClientDefaultName)
                 .AddBuilderOptionsInternal(options);
-
-            return services;
         }
 
         private static IHttpClientBuilder AddBuilderOptionsInternal(this IHttpClientBuilder builder,
             HttpClientOptions options)
         {
-            builder
+            return builder
                 .AddHttpOptions(options);
-
-            return builder;
         }
     }
 }
